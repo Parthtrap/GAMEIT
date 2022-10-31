96309c4479fe880d6514c947034c3eeeaddfbf97
Parthtrap <mttparth306@gmail.com>
2022-10-31 15:30:46 +0530

    testing API formats

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,6 +2,10 @@ import express from "express";
 const router = express.Router();
 
 // GET userinfo()
+router.get("/:id", (req, res) => {
+    console.log(req.params.id);
+    res.json({ id: req.params.id })
+})
 // PATCH edituserinfo(email, username)
 // PATCH likecommunity(email, communityid)
 // PATCH makepost(email, postid)
