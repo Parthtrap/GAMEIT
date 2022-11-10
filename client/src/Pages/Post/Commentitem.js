907492d9cf2ffeadc64670a7771d8ac5a7e578c7
Gauarv-Jain <jaing438@gmail.com>
2022-11-10 11:18:08 +0530

    Postpage changed for uniformity

--- client/src/Pages/Post/Commentitem.js
+++ client/src/Pages/Post/Commentitem.js
@@ -1,15 +1,11 @@
 export default function Commentitem(){
     return(
-        <div className="bg-divcol my-6 px-2 hover:outline hover:outline-purple-200">
+        <div className="bg-divcol rounded-lg my-6 px-2 hover:outline hover:outline-purple-200">
 
             <div className=" flex justify-between items-center ">
                 <div className=" flex items-center">
-                    <img
-                        src="https://picsum.photos/id/1025/500"
-                        alt="img"
-                        className="h-10 w-10 rounded-full mt-2"
-                    />
-                    <div className="ml-4 text-white">
+                    
+                    <div className=" text-white">
                         <span className="cursor-pointer font-bold ">Ronald Oliver</span>{' '}
                         <span>·</span>
                         <span className="text-fGrey text-opacity-50 text-sm text-white">{' '}
@@ -19,7 +15,7 @@ export default function Commentitem(){
                 </div>
             </div>
 
-            <div className="w-full  pl-14 text-white">
+            <div className="w-full  text-white">
             This is a test demo/somthing sasbkjasbkja, anjksbkjasd,sdadsadadasdas parth tu dhek rah aha
 
 
