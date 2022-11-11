f8f2c6c130446031acc2cbcdd885045515146174
Parthtrap <mttparth306@gmail.com>
2022-11-11 08:08:52 +0530

    Community Page Complete

--- client/src/Pages/Communitypage.js
+++ client/src/Pages/Communitypage.js
@@ -16,6 +16,7 @@ function Communitypage() {
     name: "Loading...",
     tagline: "Loading...",
   })
+  const [CommunityPostList, setCommunityPostList] = useState([])
 
   const [followed, setFollowed] = useState(false);
 
@@ -85,12 +86,12 @@ function Communitypage() {
     else {
       console.log(auth.user.likedcommunities.find((e) => { return e === param.id }));
       if (auth.user.likedcommunities.find((e) => { return e === param.id }) === undefined) {
-        
+
         following();
         setFollowed(true)
       }
       else {
-       
+
         unfollowing();
         setFollowed(false)
       }
@@ -174,75 +175,50 @@ function Communitypage() {
       } catch (err) {
         console.log(err.message);
       }
+
+      const PostCommunity = JSON.stringify({
+        community: param.id
+      });
+      try {
+        const response = await fetch(
+          "http://localhost:5000/api/post/community",
+          {
+            method: "POST",
+            headers: {
+              "Content-type": "application/json",
+            },
+            body: PostCommunity,
+          }
+        );
+
+        const responseData = await response.json();
+
+        if (response.status === 500) {
+          console.log(response);
+        }
+        else if (response.status === 404) {
+          setCommunityPostList([]);
+        }
+        if (response.status === 201) {
+          console.log(responseData);
+          setCommunityPostList(responseData);
+        } else {
+          console.log(responseData.error);
+        }
+      } catch (err) {
+        console.log(err.message);
+      }
     }
     getCommunityDetails();
   }, [])
 
-  const postList = [
-    {
-      id: "1",
-      community: "BadAssAF",
-      title: "Post 1",
-      content: "Title is this thingy.... Don't Question it",
-      date: "11-10-2011",
-      likes: "6969",
-      media: "https://i.imgur.com/fzR3S94.jpg",
-      ownerID: "parth@gmail.com",
-      comments: ["commentID1", "commentID2", "commentID3"],
-    },
-    {
-      id: "2",
-      community: "RandomAF",
-      title: "Post 2",
-      content: "Title is this thingy.... Don't Question it",
-      date: "12-10-2011",
-      likes: "15",
-      media: "",
-      ownerID: "lalwani@gmail.com",
-      comments: ["commentID4", "commentID5"],
-    },
-    {
-      id: "3",
-      title: "Post 3",
-      community: "KawaiiAF",
-      content: "Title is this thingy.... Don't Question it",
-      date: "13-10-2011",
-      likes: "325",
-      media: "https://i.imgur.com/ClH999d.mp4",
-      ownerID: "gaurav@gmail.com",
-      comments: ["commentID6", "commentID7"],
-    },
-    {
-      id: "4",
-      title: "Post 4",
-      community: "BoredAF",
-      content: "Title is this thingy.... Don't Question it",
-      date: "14-10-2011",
-      media: "",
-      likes: "32",
-      ownerID: "parth@gmail.com",
-      comments: ["commentID8", "commentID9", "commentID10"],
-    },
-    {
-      id: "5",
-      title: "Post 1",
-      community: "Can'tThinkOfNameAnymore",
-      content: "Title is this thingy.... Don't Question it",
-      date: "15-10-2011",
-      media: "",
-      likes: "326",
-      ownerID: "lalwani@gmail.com",
-      comments: ["commentID11"],
-    },
-  ];
-
   return (
-    <div className="w-full p-5 mt-16 bg-black md:w-3/4">
+    <div className="w-full p-5 mt-16 bg-black min-h-[91.3vh] md:w-3/4">
 
       <div className="flex items-center justify-between pr-3 space-x-4 rounded-xl bg-divcol ">
 
         <div className="flex items-center">
-          <img className="w-20 h-20 ml-4 rounded-full object-cover" src={communityDetails.imgsrc} />
+          <img className="object-cover w-20 h-20 ml-4 rounded-full" src={communityDetails.imgsrc} />
           <div className="ml-3">
             <div className="text-2xl font-bold text-purple-400">{communityDetails.name}</div>
             <div className="text-sm text-gray-500 dark:text-gray-400">{communityDetails.tagline}</div>
@@ -258,8 +234,8 @@ function Communitypage() {
 
       <div>Search Bar and Filters</div>
       <div>
-        {postList.map((post) => {
-          return <PostListCard key={post.id} post={post} />;
+        {CommunityPostList.map((post) => {
+          return <PostListCard key={post._id} post={post} />;
         })}
       </div>
     </div>
