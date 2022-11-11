e9725c6e97f056b969437650992c0c983982f22b
Parthtrap <mttparth306@gmail.com>
2022-11-11 07:33:39 +0530

    Like and Unlike API Routed

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,6 +1,6 @@
 import express from "express";
 const router = express.Router();
-import { addPost, comment, getAllPosts, getPostByCommunity, getPostByID } from "./../controllers/post.js"
+import { addPost, comment, getAllPosts, getPostByCommunity, getPostByID, likePost, unlikePost } from "./../controllers/post.js"
 
 // Get All Posts
 router.get("/get", getAllPosts);
@@ -15,10 +15,10 @@ router.post("/community", getPostByCommunity);
 router.post("/new", addPost);
 
 // PATCH likepost(postid)
-
+router.post("/like", likePost)
 
 // PATCH unlikepost(postid)
-
+router.post("/unlike", unlikePost);
 
 // Comment on a Post
 router.post("/comment", comment);
